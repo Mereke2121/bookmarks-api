2f163f5b4fe8bcd239fffbe997d0715bb234a524
a.mereke <joejordison21@gmail.com>
2023-03-22 11:59:29 +0600

    refactor db by adding rollback

--- pkg/repository/auth.go
+++ pkg/repository/auth.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"github.com/bookmarks-api/models"
 	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
 )
 
 type AuthorizationRepository struct {
@@ -14,18 +15,37 @@ func NewAuthRepository(db *sqlx.DB) Authorization {
 }
 
 func (r *AuthorizationRepository) AddUser(user *models.User) (int, error) {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return 0, errors.Wrap(err, "start db")
+	}
+
 	query := `insert into users (username, email, password_hash) values($1, $2, $3) returning id`
 
 	var id int
-	err := r.db.QueryRow(query, user.UserName, user.Email, user.Password).Scan(&id)
+	err = r.db.QueryRow(query, user.UserName, user.Email, user.Password).Scan(&id)
+	if err != nil {
+		tx.Rollback()
+		return 0, errors.Wrap(err, "scan items for db")
+	}
 
-	return id, err
+	return id, nil
 }
 
 func (r *AuthorizationRepository) GetUserId(email, password string) (int, error) {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return 0, errors.Wrap(err, "start db")
+	}
+
 	query := `select id from users where email=$1 and password_hash=$2`
 
 	var id int
-	err := r.db.Get(&id, query, email, password)
-	return id, err
+	err = r.db.Get(&id, query, email, password)
+	if err != nil {
+		tx.Rollback()
+		return 0, errors.Wrap(err, "get items from db")
+	}
+
+	return id, nil
 }
--- pkg/repository/items.go
+++ pkg/repository/items.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"github.com/bookmarks-api/models"
 	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
 )
 
 type ItemsRepository struct {
@@ -14,29 +15,71 @@ func NewItemsRepository(db *sqlx.DB) Items {
 }
 
 func (r *ItemsRepository) GetAllItems(userId int) ([]models.Item, error) {
-	var items []models.Item
+	tx, err := r.db.Begin()
+	if err != nil {
+		return nil, errors.Wrap(err, "start db")
+	}
+
 	query := `select id, url, title from bookmarks_items where user_id=$1`
 
-	err := r.db.Select(&items, query, userId)
+	var items []models.Item
+	err = r.db.Select(&items, query, userId)
+	if err != nil {
+		tx.Rollback()
+		return nil, errors.Wrap(err, "get items from db")
+	}
+
 	return items, err
 }
 
 func (r *ItemsRepository) AddItem(item *models.Item) error {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return errors.Wrap(err, "start db")
+	}
+
 	query := `insert into bookmarks_items (user_id, url, title) values($1, $2, $3)`
-	_, err := r.db.Exec(query, item.UserId, item.Url, item.Title)
+
+	_, err = r.db.Exec(query, item.UserId, item.Url, item.Title)
+	if err != nil {
+		tx.Rollback()
+		return errors.Wrap(err, "insert items into the db")
+	}
+
 	return err
 }
 
 func (r *ItemsRepository) DeleteItem(id, userId int) error {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return errors.Wrap(err, "start db")
+	}
+
 	query := `delete from bookmarks_items where id=$1 and user_id=$2`
-	_, err := r.db.Exec(query, id, userId)
+
+	_, err = r.db.Exec(query, id, userId)
+	if err != nil {
+		tx.Rollback()
+		return errors.Wrap(err, "delete items from db")
+	}
+
 	return err
 }
 
 func (r *ItemsRepository) GetItemById(id, userId int) (models.Item, error) {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return models.Item{}, errors.Wrap(err, "start db")
+	}
+
 	query := `select id, url, title from bookmarks_items where id=$1 and user_id=$2`
 
 	var item models.Item
-	err := r.db.Get(&item, query, id, userId)
+	err = r.db.Get(&item, query, id, userId)
+	if err != nil {
+		tx.Rollback()
+		return models.Item{}, errors.Wrap(err, "get items from db")
+	}
+
 	return item, err
 }
